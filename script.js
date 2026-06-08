af7f7c2b63106c2b13f32d8b977522139175b26e
Tryghun <tryghun2000@gmail.com>
2026-06-08 16:26:49 +0200

    serious 1
    Added form submission handling and success page display.

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 window.addEventListener("DOMContentLoaded", () => {
+
   const menuBtn = document.getElementById("menuBtn");
   const sidebar = document.getElementById("sidebar");
-
-  if (!menuBtn || !sidebar) {
-    console.log("Elements introuvables");
-    return;
-  }
 
   menuBtn.addEventListener("click", () => {
     sidebar.classList.toggle("active");
   });
+
+  const form = document.getElementById("devisForm");
+  const successPage = document.getElementById("successPage");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    form.style.display = "none";
+    successPage.classList.remove("hidden");
+  });
+
 });
+
+// retour accueil
+function goHome() {
+  location.reload();
+}
