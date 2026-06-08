63b5ca3067075b1d645ecca34a035cf012a5f3ca
tryghun2000-hash <tryghun2000@gmail.com>
2026-06-08 15:50:21 +0200

    V4

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const menuBtn = document.getElementById("menuBtn");
-const sidebar = document.getElementById("sidebar");
+window.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.getElementById("menuBtn");
+  const sidebar = document.getElementById("sidebar");
 
-menuBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
+  if (!menuBtn || !sidebar) {
+    console.log("Elements introuvables");
+    return;
+  }
+
+  menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+  });
 });
